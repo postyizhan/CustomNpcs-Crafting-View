3bbdbde7dbc0fddb323781485bd9044b5e711bd4
postyizhan <185839426@qq.com>
2026-09-09 01:44:34 +0800

    fix: shorten network channel for multiplayer

--- src/main/java/com/customnpcs/craftingview/network/PacketHandler.java
+++ src/main/java/com/customnpcs/craftingview/network/PacketHandler.java
@@ -1,17 +1,21 @@
 package com.customnpcs.craftingview.network;
 
-import com.customnpcs.craftingview.CraftingViewMod;
-
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import cpw.mods.fml.relauncher.Side;
 
 public class PacketHandler {
 
+    private static final String CHANNEL_NAME = "cnpcs_craftview";
+    private static final int MAX_CHANNEL_NAME_LENGTH = 20;
+
     public static SimpleNetworkWrapper CHANNEL;
 
     public static void init() {
-        CHANNEL = NetworkRegistry.INSTANCE.newSimpleChannel(CraftingViewMod.MODID);
+        if (CHANNEL_NAME.length() > MAX_CHANNEL_NAME_LENGTH) {
+            throw new IllegalStateException("Network channel name exceeds the Minecraft 1.7.10 limit: " + CHANNEL_NAME);
+        }
+        CHANNEL = NetworkRegistry.INSTANCE.newSimpleChannel(CHANNEL_NAME);
         CHANNEL.registerMessage(PacketFillCraftingGrid.Handler.class, PacketFillCraftingGrid.class, 0, Side.SERVER);
     }
 }
